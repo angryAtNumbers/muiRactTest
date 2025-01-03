cdef96eed9243fd1904e40aeff4cce21020f224b
angryAtNumbers <168223460+angryAtNumbers@users.noreply.github.com>
2025-01-03 11:12:34 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme';
-import NavBar from './NavBar';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import AuthPage from './pages/AuthPage';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/auth" element={<AuthPage />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+          <h1>Welcome, {user.username}!</h1>
+          <button onClick={signOut}>Sign Out</button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
---
+++ src/aws-exports.js
@@ -0,0 +1,10 @@
+// This file should be replaced by your Amplify configuration
+const awsmobile = {
+  aws_project_region: 'us-east-1',
+  aws_cognito_identity_pool_id: 'us-east-1:example',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_example',
+  aws_user_pools_web_client_id: 'example',
+};
+
+export default awsmobile;
